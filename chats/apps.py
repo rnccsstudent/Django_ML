cb7f6a990e88d36ffd4a686a699ba6e291b3a545
Pranab Mahata <pranabmahata406@gmail.com>
2025-01-25 22:07:20 +0530

    Initial commit

---
+++ chats/apps.py
@@ -0,0 +1,13 @@
+# chats/apps.py for creating by pranab
+#from django.apps import AppConfig
+
+#class ChatsConfig(AppConfig):
+    #default_auto_field = 'django.db.models.BigAutoField'
+   # name = 'chats'
+
+
+from django.apps import AppConfig
+
+
+class ChatsConfig(AppConfig):
+      name = 'chats'
---
+++ main_app/apps.py
@@ -0,0 +1,15 @@
+# main_app/apps.py
+from django.apps import AppConfig
+
+class MainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main_app'
+
+
+
+
+#from django.apps import AppConfig
+
+
+#class MainAppConfig(AppConfig):
+ #   name = 'main_app'
